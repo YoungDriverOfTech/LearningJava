8dde328fe98362301046a1a738d67d0b1f477be3
liangxiaole <xiaoleliang045@gmail.com>
2023-05-25 22:32:14 +0900

    update

--- src/reflection/invoke/MethodInvokeDemo.java
+++ src/reflection/invoke/MethodInvokeDemo.java
@@ -8,7 +8,7 @@ public class MethodInvokeDemo {
 
     public static void main(String[] args) throws Exception {
         /**
-         * 动态调用无参数，无返回值的方法
+         * 动态调用public无参数，无返回值的方法
          */
         // 1. 获取目标类的class对象
         MyClass obj = new MyClass(10);
@@ -22,7 +22,7 @@ public class MethodInvokeDemo {
 
 
         /**
-         * 动态调用有参数，有返回值的方法
+         * 动态调用public有参数，有返回值的方法
          */
         // 1. 获取目标类的class对象
         // 2. 获取目标方法Method对象
@@ -32,6 +32,42 @@ public class MethodInvokeDemo {
         Object result = sumMethod.invoke(obj, 20, "20");
         System.out.println("result = " + result);
 
+
+        /**
+         * 动态调用private有参数，有返回值的方法
+         */
+        // 1. 获取目标类的class对象
+        MyClass obj2 = new MyClass(10);
+        Class clazz2 = obj2.getClass();
+
+        // 2. 获取目标方法Method对象
+        Method sumMethod2 = clazz2.getDeclaredMethod("sum", String.class, String.class);
+
+        // 3. 设置可访问flag
+        sumMethod2.setAccessible(true);
+
+        // 4. 调用invoke方法
+        Object result2 = sumMethod2.invoke(obj2, "1", "2");
+        System.out.println("result2 = " + result2);
+
+
+        /**
+         * 动态调用静态方法
+         */
+        // 1. 获取目标类的class对象
+        MyClass obj3 = new MyClass(10);
+        Class clazz3 = obj3.getClass();
+
+        // 2. 获取目标方法Method对象
+        Method sumMethod3 = clazz3.getDeclaredMethod("sum", String.class);
+
+        // 3. 设置可访问flag
+        sumMethod3.setAccessible(true);
+
+        // 4. 调用invoke方法
+        // Object result3 = sumMethod3.invoke(obj3, "1"); // 传对象也行，因为对象也能调用静态方法
+        Object result3 = sumMethod3.invoke(null, "1"); // 传入null，调用的也是静态方法，因为静态方法是属于类的
+        System.out.println("result3 = " + result3);
 
     }
 }
--- src/reflection/invoke/MyClass.java
+++ src/reflection/invoke/MyClass.java
@@ -15,7 +15,15 @@ public class MyClass {
         System.out.println("do nothing");
     }
 
-    public int sum (int num1, String strNum) {
+    public int sum(int num1, String strNum) {
         return num1 + Integer.parseInt(strNum) + this.count;
+    }
+
+    private String sum(String num1, String num2) {
+        return "" + Integer.parseInt(num1) + Integer.parseInt(num2) + this.count;
+    }
+
+    private static String sum(String str) {
+        return str + "world";
     }
 }
